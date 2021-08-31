d48325289e196c7619f1f5eeaf99b7ff46237627
aa-nadim <nadim.ice.nstu@gmail.com>
2021-08-31 13:43:04 +0600

    Data-Structure_and_Algorithms

---
+++ Maximum-Subarray-Sum/subarray sum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findSubarraySum(int arr[], int n, int sum)
+{
+	map<int, int> prevSum;
+	int res = 0;
+	int currsum = 0;
+	for (int i = 0; i < n; i++) {
+		currsum += arr[i];
+		if (currsum == sum) res++;
+
+		if (prevSum.find(currsum - sum) != prevSum.end())   res += (prevSum[currsum - sum]);
+
+		prevSum[currsum]++;
+	}
+	return res;
+}
+
+int main()
+{
+	int arr[] = { 10, 2, -2, -20, 10 };
+	int sum = -10;
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << findSubarraySum(arr, n, sum);
+	return 0;
+}
