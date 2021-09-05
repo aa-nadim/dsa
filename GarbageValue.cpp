d8f6cd669954ede2b3786f600a09f771333c8926
aa-nadim <nadim.ice.nstu@gmail.com>
2021-09-05 23:52:44 +0600

    DFS practice

---
+++ GarbageValue.cpp
@@ -0,0 +1,31 @@
+///................................Nadim.................................///
+///..............Noakhali Science and Technology University..............///
+///........Department of Information and Communication Engineering.......///
+#include<bits/stdc++.h>
+#include<iostream>
+#include<climits>
+using namespace std;
+#define    IOS            ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define    MX             1000000000
+#define    fill(a,b)      memset(a,b,sizeof(a))
+#define    gcd(a,b)     __gcd(a,b)
+#define    lcm(a,b)       (a*(b/gcd(a,b)))
+#define    ll             long long int
+
+int main(){
+#ifdef OJ
+        freopen("inputf.in", "r", stdin);
+        freopen("outputf.in", "w", stdout);
+#endif
+    //ll test;cin>>test;while(test--)
+        {
+
+            int a,b=9;
+            cin>>a>>b;
+            double x;
+            x=(double)(a+b)/2;
+            cout<<x<<endl;
+
+        }
+return 0;
+}
